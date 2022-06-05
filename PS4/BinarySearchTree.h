712dcbaf4965edb7a3e706803d3043e300445b12
Andr3a-Co <72954050+Andr3a-Co@users.noreply.github.com>
2022-06-05 16:17:24 +1000

    Update BinarySearchTree.h

--- PS4/BinarySearchTree.h
+++ PS4/BinarySearchTree.h
@@ -10,60 +10,87 @@ class BinarySearchTree
 private:
     using BNode = BinaryTreeNode<T>;
     using BTreeNode = BNode*;
+  
     BTreeNode fRoot;
-
+    
 public:
-    BinarySearchTree() : fRoot(&BNode::NIL) {}
-
-
-    ~BinarySearchTree() {
-
-        fRoot->~BinaryTreeNode();
+    BinarySearchTree()
+    {
+        fRoot = &BNode::NIL;                                    
     }
-    bool empty() const {
-
-        return fRoot->empty();
-    }
-    size_t height() const {
-
-        return fRoot->height();        
-    }
-
-    bool insert(const T& aKey) {        
-
-
-        if (empty()) {
-            fRoot = new BNode(aKey);
-            return true;
+    ~BinarySearchTree()
+    {
+        if ( !empty() )                                         
+        {
+            delete fRoot;                                       
         }
-        else {
-            return fRoot->insert(aKey);
-
-        }
-
     }
-    bool remove(const T& aKey) {
-
-        if (fRoot->leaf()) {
-            fRoot = &BNode::NIL;
-            return true;
-              
-        }
-        return fRoot->remove(aKey, &BNode::NIL);
+    bool empty() const
+    {
+        return fRoot == &BNode::NIL;                            
     }
-
+    
+    size_t height() const
+    {
+        // The empty tree has no height.
+        if ( empty() )
+        {
+            throw std::domain_error( "Empty tree has no height." );
+        }
+        
+        return fRoot->height();                                 
+    }
+    
+    bool insert( const T& aKey )
+    {
+        if ( empty() )                                          
+        {
+            fRoot = new BNode( aKey );                          
+            return true;                                        
+        }
+        else
+        {
+            return fRoot->insert( aKey );                       
+        }
+    }
+    bool remove( const T& aKey )
+    {
+        if ( empty() )
+        {
+            throw std::domain_error( "remove(): NIL encountered." );
+        }
+        
+        if ( fRoot->leaf() )
+        {
+            // last node
+            if ( fRoot->key == aKey )
+            {
+                delete fRoot;
+                fRoot = &BNode::NIL;
+                
+                return true;
+            }
+            
+            return false;
+        }
+        else
+        {
+            return fRoot->remove( aKey, &BNode::NIL );          
+        }
+    }                                                           
     // Problem 3 methods
-
+    
     using Iterator = BinarySearchTreeIterator<T>;
     // Allow iterator to access private member variables
     friend class BinarySearchTreeIterator<T>;
-    Iterator begin() const {
-
-        return Iterator (*this);
-       
+    
+    Iterator begin() const
+    {
+        return Iterator( *this );                               
     }
-    Iterator end() const {
-
-        return begin().end();
+    
+    Iterator end() const
+    {
+        return begin().end();                                   
     }
 };
