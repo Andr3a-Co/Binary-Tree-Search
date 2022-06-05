7de06e6fc50df759a65a66f12288d1199df05c3f
Andr3a-Co <72954050+Andr3a-Co@users.noreply.github.com>
2022-06-05 16:14:30 +1000

    Update BinarySearchTreeIterator.h

--- PS4/BinarySearchTreeIterator.h
+++ PS4/BinarySearchTreeIterator.h
@@ -5,101 +5,81 @@ template<typename T>
 class BinarySearchTreeIterator
 {
 private:
-
+    
     using BSTree = BinarySearchTree<T>;
     using BNode = BinaryTreeNode<T>;
     using BTreeNode = BNode*;
     using BTNStack = std::stack<BTreeNode>;
-    const BSTree& fBSTree; // binary search tree
-    BTNStack fStack; // DFS traversal stack
-
-    void pushLeft(BTreeNode aNode) {
-
-        while (!aNode->empty()) {
-            //pushleft push the node onto the traversal stack 
-            //set aNode to its leftNode 
-            fStack.push(aNode);
-            aNode = aNode->left;                       
+    const BSTree& fBSTree;
+    BTNStack fStack;
+    
+    void pushLeft( BTreeNode aNode )
+    {
+        while ( !aNode->empty() )                               
+        {
+            fStack.push( aNode );                               
+            aNode = aNode->left;                                
         }
     }
-
+    
 public:
-
+    
     using Iterator = BinarySearchTreeIterator<T>;
-
-    //constructor
-    BinarySearchTreeIterator(const BSTree& aBSTree) : fBSTree (aBSTree) {}
-
-    //dereference 
-    const T& operator*() const {
-
-        return *fBSTree.begin();
-
+    
+    BinarySearchTreeIterator( const BSTree& aBSTree ) :
+        fBSTree(aBSTree)                                        
+    {
+        pushLeft( fBSTree.fRoot );                              
     }
-
-    //prefix increment
-    Iterator& operator++() {
-
-        if (!fStack.empty()) {
-           
-        pushLeft(fBSTree.fRoot); // Go to the left most descendant
-        fBSTree.fRoot->left = fStack.top(); // Left most node (leaf)
-        fStack.pop(); // removes top from the stack
+    const T& operator*() const
+    {
+        return fStack.top()->key;                               
+    }
+    Iterator& operator++()
+    {
+        BTreeNode lNode = fStack.top()->right;                  
         
-            if (fBSTree.fRoot->right->empty()) {
-            
-                fBSTree.fRoot->right = fStack.top(); // Left most node (leaf)
-                fStack.pop(); // removes top from the stack
-            }
-            else {
-
-                fBSTree.fRoot->right = fBSTree.fRoot->right; //go right   
-                fStack.push(fBSTree.fRoot);
-
-            }          
+        fStack.pop();                                           
         
-
-        }
-
-        return *this;
-
+        pushLeft( lNode );                                      
+        
+        return *this;                                           
     }
-
-    // postfix increment
-    Iterator operator++(int) {
-
-        Iterator i = *this;
-
-        ++(*this);
-
-        return i;
+    Iterator operator++(int)
+    {
+        Iterator old = *this;                                   
+        
+        ++(*this);                                              
+        
+        return old;                                             
     }
-
-    // equivalence
-    bool operator==(const Iterator& aOtherIter) const {
-
-        return fBSTree.fRoot == aOtherIter.fBSTree.fRoot;
+    bool operator==( const Iterator& aOtherIter ) const
+    {
+        return
+            &fBSTree == &aOtherIter.fBSTree &&                  
+            fStack.size() == aOtherIter.fStack.size();         
     }
-
-    //not equal
-    bool operator!=(const Iterator& aOtherIter) const {
-
-        return !(*this == aOtherIter);
+    bool operator!=( const Iterator& aOtherIter ) const
+    {
+        return !(*this == aOtherIter);                          
     }
-
-    // first element forward
-    Iterator begin() const {
-
-        Iterator result = *this;
-
-        result.fBSTree.fRoot; //first element     
-
-        return result;
+    
+    Iterator begin() const
+    {
+        Iterator iter = *this;                                 
+        
+        iter.fStack = BTNStack();                              
+        iter.pushLeft( iter.fRoot );                            
+        
+        return iter;                                            
     }
-
-    // last element forward
-    Iterator end() const {
-
-        return *this;
+    
+    Iterator end() const
+    {
+        Iterator iter = *this;                                  
+        
+        iter.fStack = BTNStack();                               
+        
+        return iter;                                            
     }
-};
+}; 
